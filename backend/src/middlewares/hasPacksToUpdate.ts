5266a9daeb2d5e53b90a7e121240db259dcebd42
Guilherme Fernandes <gfengquimica@gmail.com>
2023-05-15 20:32:00 -0300

    feat: criação do middleware para avaliação se tem packs

--- backend/src/middlewares/hasPacksToUpdate.ts
+++ backend/src/middlewares/hasPacksToUpdate.ts
@@ -8,17 +8,16 @@ const packsModel = new PacksModel();
 const packsService = new PacksService(packsModel);
 
 interface NewRequest extends Request {
-  foundPacks?: IPacks,
+  foundPack?: IPacks,
 }
 
 export default async (req: NewRequest, _res: Response, next: NextFunction) => {
   const { product_code: productCode } = req.body;
-
   const foundPacks = await packsService.findByProductId(productCode);
-  console.log(foundPacks.data);
 
   if (foundPacks.data) {
-    req.foundPacks = foundPacks.data;
+    const { id, packId, productId, qty } = foundPacks.data as unknown as IPacks;
+    req.foundPack = { id, packId, productId, qty };
   }
 
   next();
