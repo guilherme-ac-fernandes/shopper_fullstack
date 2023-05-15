e138435b560bd0cd729c206235db878446f0db2a
Guilherme Fernandes <gfengquimica@gmail.com>
2023-05-15 17:27:00 -0300

    feat: criação da camada de controladores para criação e atualização do preço de venda

--- backend/src/controllers/ProductsController.ts
+++ backend/src/controllers/ProductsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { IProduct } from '../interfaces/IProduct';
 import ProductsService from '../services/ProductsService';
 
 export default class CarController {
@@ -18,5 +19,29 @@ export default class CarController {
     const { code, message, data } = await this._product.findAll();
     if (message) return next({ code, message });
     return res.status(code).json(data);
+  }
+
+  public async create(req: Request, res: Response, next: NextFunction) {
+    const { code, message, data } = await this._product.create(req.body);
+    if (message) return next({ code, message });
+    return res.status(code).json(data);
+  }
+
+  public async updateSalesPrice(req: Request, res: Response, next: NextFunction) {
+    const { product_code: productCode, new_price: newPrice } = req.body;
+
+    const {
+      code, message, data,
+    } = await this._product.findByCode(Number(productCode));
+    if (message) return next({ code, message });
+
+    try {
+      const {
+        code: updateCode, data: updateData,
+      } = await this._product.updateSalesPrice(data as IProduct, Number(newPrice));
+      return res.status(updateCode).json(updateData);
+    } catch (error) {
+      return { code: 500, message: 'Internal server error' };
+    }
   }
 }
