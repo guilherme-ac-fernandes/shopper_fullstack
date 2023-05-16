70e7b0b6d8293a668971eb2174b889def57729f6
Guilherme Fernandes <gfengquimica@gmail.com>
2023-05-15 22:42:25 -0300

    feat: estruturação da página de produtos

---
+++ frontend/src/pages/Products/Products.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { getAllProducts } from '../../api_request/productsRequests';
+import Header from '../../components/Header/Header';
+import Modal from '../../components/Modal/Modal';
+import Table from '../../components/Table/Table';
+import useModal from '../../hooks/useModal';
+import { IProduct } from '../../interfaces/IProduct';
+import './Products.css';
+
+function Products() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [product, setProduct] = useState<IProduct>();
+
+  const { isOpen, toggle } = useModal();
+
+  useEffect(() => {
+    setLoading(true);
+    getAllProducts().then((allProducts) => setProducts(allProducts));
+    setLoading(false);
+  }, []);
+
+  const clickModal = ({ code: codeProduct }: IProduct) => {
+    setProduct(products.find(({ code }) => code === codeProduct));
+    toggle();
+  };
+
+  return (
+    <section>
+      <Header />
+      <Table
+        loading={loading}
+        products={products}
+        clickModal={clickModal}
+      />
+      <Modal
+        isOpen={isOpen}
+        toggle={toggle}
+        product={product as IProduct}
+      />
+    </section>
+  );
+}
+
+export default Products;
